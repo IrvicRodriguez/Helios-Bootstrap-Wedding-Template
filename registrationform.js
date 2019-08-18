beb3ede9713b322f8472f01f29cec809be769ee3
Irvic <irvic@irvicrodriguez.com>
2019-08-17 23:21:50 -0500

    updates to wedding website and english version added

---
+++ registrationform.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  if ($('#registrationform').length > 0 ) {
+  contactScript('forcontact');
+}
+});
+//firebase
+function contactScript(value) {
+  var config = {
+    apiKey: "AIzaSyCJPHoPH_dBAvw8FoZNTqA22N2HgNfnFfg",
+    authDomain: "fir-sps-f7bec.firebaseapp.com",
+    databaseURL: "https://fir-sps-f7bec.firebaseio.com",
+    projectId: "fir-sps-f7bec",
+    storageBucket: "",
+    messagingSenderId: "632529267290",
+    appId: "1:632529267290:web:bf60e09278059c27"
+  };
+  firebase.initializeApp(config);
+  var b = firebase.database();
+  $("#registrationform").submit(function(a) {$(this), 
+    console.log("Submit to Firebase");
+    var c = $("#weddingName").val(),
+      cc = $("#WeddinglastName").val(),
+      d = $("#invitees").val(),
+      f = { name: c, lastname: cc,invitees: d};
+    console.log(f);
+    return firebase.database().ref().push(f);
+})};
+function myFunction() {
+  document.getElementById("registrationform").reset();
+}
